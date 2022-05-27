f6d2762fafbcfaba09891fdb528199792aac7100
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-05-27 15:45:18 -0300

    Tentativa falha semáforo.

--- src/thread_managers.py
+++ src/thread_managers.py
@@ -2,7 +2,7 @@ from file_interpreters import ConfigInterpreter
 from custom_sockets import UDPClientSocket, UDPServerSocket
 from packaging import ErrorControlTypes, TokenPacket, DataPacket, PacketIdentifier, CRC32
 from queue import Queue
-from threading import Thread
+from threading import Thread, Semaphore
 
 
 # Essa classe gerencia as threads de cliente e servidor.
@@ -16,10 +16,14 @@ class SocketThreadManager:
         self.config = config
         self.client = client
         self.server = server
+        self.token = TokenPacket() if config["isTokenTrue"] else None
         self.messagesQueue = Queue()
+        # self.semaphore = Semaphore(1)
 
     def __clientThread(self) -> None:
         while True:
+            # Espera semáforo.
+            # self.semaphore.acquire()
             message = input("\nMensagem a ser enviada: ")
             destinationNickname = input("Apelido do destino: ")
             dataPacket = DataPacket(
@@ -29,21 +33,29 @@ class SocketThreadManager:
                 -1,
                 message
             )
-            # ! Falta colocar o CRC !
+            # TODO: Colocar o CRC ^
             self.messagesQueue.put(dataPacket.toString())
             # Se estiver com o token, envia primeira mensagem da fila:
-            hasToken = True  # Mockado.
-            if hasToken:
-                # Enviando pacote de dados, mas poderia ser de token (implementar)!
+            if self.token is not None:
                 if not self.messagesQueue.empty():
                     self.client.send(self.messagesQueue.get())
+                else:
+                    self.client.send(self.token.toString())
+                    self.token = None
+            # Libera semáforo.
+            # self.semaphore.release()
 
     def __serverThread(self) -> None:
         while True:
+            # Espera semáforo.
+            # self.semaphore.acquire()
             packetString = self.server.listen()
             packetType = PacketIdentifier.identify(packetString)
             if packetType == PacketIdentifier.TOKEN:
-                pass
+                self.token = TokenPacket()
+                if self.messagesQueue.empty():
+                    self.messagesQueue.put(self.token.toString())
+                    # self.client.send(self.token.toString())
             elif packetType == PacketIdentifier.DATA:
                 dataPacket = DataPacket.fromString(packetString)
                 if dataPacket.destinationNickname == self.config["nickname"]:
@@ -51,8 +63,11 @@ class SocketThreadManager:
                     print(
                         f"\n[Pacote recebido]\n\nMensagem: {dataPacket.message}\nApelido da origem: {dataPacket.originNickname}\n")
                 else:
-                    # Verificar se isso está certo.
+                    # Verificar qual das duas linhas está certa.
                     self.messagesQueue.put(packetString)
+                    # self.client.send(packetString)
+            # Libera semáforo.
+            # self.semaphore.release()
 
     def startThreads(self) -> None:
         Thread(target=self.__clientThread).start()
