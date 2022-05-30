98d7b8c8049fd32c448656dd28c29fa3f562dd8a
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-05-30 14:04:45 -0300

    Cant check if it is working.

--- src/thread_managers.py
+++ src/thread_managers.py
@@ -19,11 +19,11 @@ class SocketThreadManager:
         self.server = server
         self.token = TokenPacket() if config["isTokenTrue"] else None
         self.messagesQueue = Queue()
-        self.waiting = False if self.token else True
+        self.waiting = False if self.token is not None else True
 
     def __socketsThread(self) -> None:
         while True:
-            # Tenho o token:
+            # Se tenho o token:
             if self.token is not None:
                 if self.messagesQueue.empty() and not self.waiting:
                     self.client.send(self.token.toString())
@@ -32,41 +32,37 @@ class SocketThreadManager:
                 elif not self.waiting:
                     self.client.send(self.messagesQueue.get())
                     self.waiting = True
-            # Não tenho o token:
-            else:
-                packetString = self.server.receive()
-                packetType = PacketIdentifier.identify(packetString)
-                # Recebi token:
-                if packetType == PacketIdentifier.TOKEN:
-                    self.token = TokenPacket()
-                    self.waiting = False
-                    continue
-                # Recebi dados:
-                elif packetType == PacketIdentifier.DATA:
-                    dataPacket = DataPacket.fromString(packetString)
-                    # Sou o destino:
-                    if dataPacket.destinationNickname == self.config["nickname"]:
-                        isCRCCorrect = CRC32.check(dataPacket)
-                        if isCRCCorrect:
-                            print(
-                                f"\n[Pacote recebido]\n\nMensagem: {dataPacket.message}\nApelido da origem: {dataPacket.originNickname}\n")
-                        else:
-                            print(
-                                f"Pacote recebido de {dataPacket.originNickname}, porém o CRC não bate. Descartando pacote...")
-                    # Sou a origem:
-                    elif dataPacket.originNickname == self.config["nickname"]:
-                        self.waiting = False
-                        # Verificar controle de erro...
-                        pass
+            packetString = self.server.receive()
+            packetType = PacketIdentifier.identify(packetString)
+            # Recebi token:
+            if packetType == PacketIdentifier.TOKEN:
+                self.token = TokenPacket()
+                self.waiting = False
+                continue
+            # Recebi dados:
+            elif packetType == PacketIdentifier.DATA:
+                dataPacket = DataPacket.fromString(packetString)
+                # Sou o destino:
+                if dataPacket.destinationNickname == self.config["nickname"]:
+                    isCRCCorrect = CRC32.check(dataPacket)
+                    if isCRCCorrect:
+                        print(
+                            f"\n[Pacote recebido]\n\nMensagem: {dataPacket.message}\nApelido da origem: {dataPacket.originNickname}\n")
                     else:
-                        self.client.send(packetString)
+                        print(
+                            f"Pacote recebido de {dataPacket.originNickname}, porém o CRC não bate. Descartando pacote...")
+                # Sou a origem:
+                elif dataPacket.originNickname == self.config["nickname"]:
+                    self.waiting = False
+                    # Verificar controle de erro...
+                    pass
+                else:
+                    self.client.send(packetString)
 
     def __inputThread(self) -> None:
         while True:
-            # message = input("\nMensagem a ser enviada: ")
-            # destinationNickname = input("Apelido do destino: ")
-            message = "Hello!"
-            destinationNickname = "Bob"
+            message = input("\nMensagem a ser enviada: ")
+            destinationNickname = input("Apelido do destino: ")
             crc = CRC32.calculate(message)
             dataPacket = DataPacket(
                 ErrorControlTypes.MACHINE_DOES_NOT_EXIST,
