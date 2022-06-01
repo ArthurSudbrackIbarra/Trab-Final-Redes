f2f6b9a7d1a7fc9702f16f71c1d0440fb8d39ff3
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-06-01 15:45:47 -0300

    Update batch and bash scripts.

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
-start python "src/main.py src/config/config-1.txt 9200"
-start python "src/main.py src/config/config-2.txt 9000"
-start python "src/main.py src/config/config-3.txt 9100"
+@echo off
+
+:: start python "src/main.py src/config/config-1.txt 9200"
+:: start python "src/main.py src/config/config-2.txt 9000"
+:: start python "src/main.py src/config/config-3.txt 9100"
+
+cd src/
+python main.py
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-python3 src/main.py src/config/config-1.txt 9200 &
-python3 src/main.py src/config/config-2.txt 9000 &
-python3 src/main.py src/config/config-3.txt 9100
+
+# python3 src/main.py src/config/config-1.txt 9200 &
+# python3 src/main.py src/config/config-2.txt 9000 &
+# python3 src/main.py src/config/config-3.txt 9100
+
+cd src/
+python3 main.py
